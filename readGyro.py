fd13f7f7e090105dbd7eb2e62632876192b954cf
ibarami <hy.ibarami@gmail.com>
2022-07-23 17:09:27 +0900

    code의 가독성을 증가시킴

--- readGyro.py
+++ readGyro.py
@@ -1,42 +1,44 @@
 # coding: utf-8
 
-import header
-import time
-import sys
-import math
-import numpy as np
-from glob import glob
 import git
+from glob import glob
+import numpy as np
+import math
 import time
+import header
 
-IsBaramiOpen=1 #0이면 닫혀있다는 의미다. 임의로 설정하였다.
-path_git_repo=r'/home/pi/ibarami.github.io/'
-commit_message='change condition'
-init_run=False#처음에는 바라미실이 열렸는지 안 열렸는지 확인해야하니 돌리자
+OPEN = 1
+CLOSE = 0
+IsBaramiOpen = 1  # 0이면 닫혀있다는 의미다. 임의로 설정하였다.
+path_git_repo = r'/home/pi/ibarami.github.io/'
+Gyro_Threshold = 400  # Open Close를 판단하는 기준
+# commit_message = 'change condition'
+INIT_RUN = True  # 처음에는 바라미실이 열렸는지 안 열렸는지 확인해야하니 돌리자
+
 
 def pushToGit(OpenCheck):
     global IsBaramiOpen
-    global init_run
-    if (IsBaramiOpen != OpenCheck) or (init_run is False):
-        readme=open("/home/pi/ibarami.github.io/index.md",'r+t',encoding='utf-8')
-        edit=readme.read()
-        #print(edit)
+    global INIT_RUN
+    if (IsBaramiOpen != OpenCheck) or (INIT_RUN is True):
+        readme = open("/home/pi/ibarami.github.io/index.md",
+                      'r+t', encoding='utf-8')
+        edit = readme.read()
         readme.close()
-        if init_run is False:
-            barami_message=-1
-        init_run=True
-        if OpenCheck is 0:
-            edit=edit.replace('**열림**','**닫힘**')
-            IsBaramiOpen=OpenCheck
-            barami_message=0
-        else:
-            edit=edit.replace('**닫힘**','**열림**')
-            #readme.write("열림")
-            IsBaramiOpen=OpenCheck
-            barami_message=1
-        #print("==================")
-        #print(edit)
-        readme=open("/home/pi/ibarami.github.io/index.md",'w+',encoding='utf-8')
+        if INIT_RUN is True:
+            barami_message = -1
+            INIT_RUN = False
+
+        if OpenCheck is CLOSE:
+            edit = edit.replace('**열림**', '**닫힘**')
+            IsBaramiOpen = OpenCheck
+            barami_message = CLOSE
+        else:  # OpenCheck is OPEN
+            edit = edit.replace('**닫힘**', '**열림**')
+            IsBaramiOpen = OpenCheck
+            barami_message = OPEN
+
+        readme = open("/home/pi/ibarami.github.io/index.md",
+                      'w+', encoding='utf-8')
         readme.write(edit)
         readme.close()
         while True:
@@ -46,23 +48,25 @@ def pushToGit(OpenCheck):
             except IOError:
                 pass
 
-            #안 될 것 같으면 except Exception을 추가해보자
+            # 안 될 것 같으면 except Exception을 추가해보자
+
 
 def git_function(barami_status):
-    repo=git.Repo(path_git_repo)
+    repo = git.Repo(path_git_repo)
     repo.git.add(update=True)
     if barami_status is -1:
-        git_message='init run'
+        git_message = 'init run'
 
-    elif barami_status is 0:
-        git_message='닫힘'
+    elif barami_status is CLOSE:
+        git_message = '닫힘'
 
     else:
-        git_message='열림'
+        git_message = '열림'
 
-    changed_time=time.strftime('%Y-%m-%d %H:%M:%S '+git_message, time.localtime(time.time()))
+    changed_time = time.strftime(
+        '%Y-%m-%d %H:%M:%S '+git_message, time.localtime(time.time()))
     repo.index.commit(changed_time)
-    origin=repo.remote(name='origin')
+    origin = repo.remote(name='origin')
     while 1:
         try:
             origin.push()
@@ -80,30 +84,30 @@ def git_function(barami_status):
         #     print("Some error occured while pushing the code")
 
 
-if __name__== "__main__":
-    check=0
-    mag_list=list()
+if __name__ == "__main__":
+    check = 0
+    mag_list = list()
 
     while True:
         mpu9250 = header.MPU9250()
-        mag=mpu9250.readMagnet()
-        mag_total=math.sqrt(mag['x']**2+mag['y']**2+mag['z']**2)
+        mag = mpu9250.readMagnet()
+        mag_total = math.sqrt(mag['x']**2+mag['y']**2+mag['z']**2)
         time.sleep(1)
 
-        if check<3:
+        if check < 3:
             mag_list.append(mag_total)
-            check+=1
+            check += 1
 
         else:
-            mag_avg=np.mean(mag_list)
-            #print(mag_avg)
-            if mag_avg >1200:
-                #print("close")
-                pushToGit(0)
+            mag_avg = np.mean(mag_list)
+            # print(mag_avg)
+            if mag_avg > Gyro_Threshold:
+                # print("close")
+                pushToGit(CLOSE)
             else:
-                #print("open")
-                pushToGit(1)
+                # print("open")
+                pushToGit(OPEN)
 
             del(mag_list)
-            mag_list=list()
-            check=1
+            mag_list = list()
+            check = 1

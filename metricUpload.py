2c6a9ac9db1f12102158ad068a262b4ff9f083be
Dictor <kimdictor@gmail.com>
2022-09-17 23:25:18 +0900

    하트비트, 로그를 갱신하는 서버 API에 대응하는 로직 추가
    - ADD: 예시 config.ini 파일
    - ADD: pip requirements 파일
    - ADD: 설정 파일 파싱, 하트비트 갱신, 로그 생신하는 메소드를 포함하는 metricUpload.py
    - ADD: 메인 어플리케이션 로직에 추가

---
+++ metricUpload.py
@@ -0,0 +1,44 @@
+import configparser
+from httplib2 import Http
+from urllib.parse import urlencode
+
+configExist = False
+configHeartbeatUrl = ''
+configLogUrl = ''
+
+
+def parseConfigure():
+    global configExist, configHeartbeatUrl, configLogUrl
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    if not 'Common' in config:
+        configExist = False
+    else:
+        configHeartbeatUrl = config['Common'].get('configLogUrl', '')
+        configLogUrl = config['Common'].get('LogUrl', '')
+        if configLogUrl == '' or configHeartbeatUrl == '':
+            configExist = False
+        else:
+            configExist = True
+            print("log url={0}, hb url={1}".format(configLogUrl, configHeartbeatUrl))
+    print("config exist: {0}".format(configExist))
+    return
+
+
+def uploadHeartbeat():
+    h = Http()
+    if not configExist:
+        return
+    h.request(configHeartbeatUrl, "GET")
+    print("heartbeat sent to {0}".format(configHeartbeatUrl))
+    return
+
+def uploadErrorLog(level, str):
+    h = Http()
+    if not configExist:
+        return
+    data = {'level': level, 'data': str}
+    h.request(configLogUrl, "POST", urlencode(data))
+    print("log sent to {0}".format(configLogUrl))
+    return
